b16f209ad260a173294914972cbd1dd1bb0dfa8a
Gymnophoria <buddercreeperman@gmail.com>
2019-12-04 11:43:54 -0800

    Day 1 part 2

--- Day 1/index.js
+++ Day 1/index.js
@@ -10,3 +10,17 @@ modules.forEach((mass, i) => {
 });
 
 console.log(`Part 1, sum of fuel is: ${sum}`);
+
+sum = 0;
+
+modules.forEach((mass, i) => {
+    mass = modules[i] = parseInt(mass);
+    let fuel = Math.floor(mass / 3) - 2;
+
+    while (fuel > 0) {
+        sum += fuel;
+        fuel = Math.floor(fuel / 3) - 2;
+    }
+});
+
+console.log(`Part 2, sum of fuel is: ${sum}`);
